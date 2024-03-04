0e32feed16e0bfac526016b01d924aee4b3465e2
IliHanSoLow <ilian-ode@gmx.de>
2024-03-04 18:53:11 +0100

    53 current  2024-03-04 18:53:07  24.05.20240229.1536926       6.6.18                          *

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -214,4 +214,17 @@ in {
       "ilian" = import ./ilian.nix;
     };
   };
+
+  boot.kernelParams = ["quiet" "splash" "amdgpu.backlight=0"];
+  boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+    pname = "distro-grub-themes";
+    version = "3.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "AdisonCavani";
+      repo = "distro-grub-themes";
+      rev = "v3.1";
+      hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+    };
+    installPhase = "cp -r customize/nixos $out";
+  };
 }
