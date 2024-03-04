2e93fabfe61feea8d51ddf0da8a5f0bc3646f9f3
IliHanSoLow <ilian-ode@gmx.de>
2024-03-04 22:25:15 +0100

    66 current  2024-03-04 22:25:09  24.05.20240229.1536926       6.6.18                          *

--- nixos/hosts/default/ilian.nix
+++ nixos/hosts/default/ilian.nix
@@ -40,6 +40,7 @@
     pkgs.starship
     pkgs.zoxide
     pkgs.direnv
+    pkgs.zathura
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
