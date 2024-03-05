9c7530ad0801ddbb92ea54776d199177040fa80c
IliHanSoLow <ilian-ode@gmx.de>
2024-03-05 10:55:14 +0100

    70 current  2024-03-05 10:55:05  24.05.20240229.1536926       6.6.18                          *

--- nixos/hosts/default/ilian.nix
+++ nixos/hosts/default/ilian.nix
@@ -41,6 +41,7 @@
     pkgs.zoxide
     pkgs.direnv
     pkgs.zathura
+    pkgs.jdk21
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
