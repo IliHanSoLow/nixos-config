97013ebc9e4b949a1323aa619550422f58bd75d7
IliHanSoLow <ilian-ode@gmx.de>
2024-03-05 11:02:25 +0100

    72 current  2024-03-05 11:02:17  24.05.20240229.1536926       6.6.18                          *

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -22,12 +22,12 @@ in {
   boot.loader.timeout = 4;
   boot.initrd.enable = true;
   boot.initrd.systemd.enable = true;
-  # boot.plymouth = {
-  #   enable = true;
-  #   font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-  #   themePackages = [pkgs.catppuccin-plymouth];
-  #   theme = "catppuccin-macchiato";
-  # };
+  boot.plymouth = {
+    enable = true;
+    font = "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
+    themePackages = [pkgs.catppuccin-plymouth];
+    theme = "catppuccin-macchiato";
+  };
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
