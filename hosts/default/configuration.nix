d640e18fa8c3fc24761c519359d7acd5210968fc
IliHanSoLow <ilian-ode@gmx.de>
2024-03-13 15:53:04 +0100

    awesome wm and gdm

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -68,12 +68,12 @@ in {
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  # Awesome WM
-  services.xserver = {
-    displayManager.gdm.session = [
+  services.xserver.displayManager = {
+    gdm = {
+      enable = true;
+      wayland = true;
+    };
+    session = [
       {
         manage = "desktop";
         name = "awesomeSession";
@@ -83,13 +83,14 @@ in {
         '';
       }
     ];
-
-    windowManager.awesome = {
-      enable = true;
-      luaModules = with pkgs.luaPackages; [
-        luarocks
-      ];
-    };
+  };
+  services.xserver.desktopManager.gnome.enable = true;
+  # Awesome WM
+  services.xserver.windowManager.awesome = {
+    enable = true;
+    luaModules = with pkgs.luaPackages; [
+      luarocks
+    ];
   };
 
   # Configure keymap in X11
