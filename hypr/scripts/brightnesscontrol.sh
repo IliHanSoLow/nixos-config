55630956a58f9a0834ac4007a20796b9582a28e2
IliHanSoLow <ilian-ode@gmx.de>
2024-04-04 11:38:32 +0200

    brightnesscontroll changed

--- hypr/scripts/brightnesscontrol.sh
+++ hypr/scripts/brightnesscontrol.sh
@@ -31,26 +31,26 @@ i)  # increase the backlight
     if [[ $(get_brightness) -lt 10 ]] ; then
         # increase the backlight by 1% if less than 10%
         brightnessctl set +1%
-        brightnessctl -d amdgpu_bl0 set +1%
+        brightnessctl -d amdgpu_bl1 set +1%
     else
         # increase the backlight by 5% otherwise
         brightnessctl set +5%
-        brightnessctl -d amdgpu_bl0 set +5%
+        brightnessctl -d amdgpu_bl1 set +5%
     fi
     send_notification ;;
 d)  # decrease the backlight
     if [[ $(get_brightness) -le 1 ]] ; then
         # avoid 0% brightness
         brightnessctl set 1%
-        brightnessctl -d amdgpu_bl0 set set 1%
+        brightnessctl -d amdgpu_bl1 set set 1%
     elif [[ $(get_brightness) -le 10 ]] ; then
         # decrease the backlight by 1% if less than 10%
         brightnessctl set 1%-
-        brightnessctl -d amdgpu_bl0 set 1%-
+        brightnessctl -d amdgpu_bl1 set 1%-
     else
         # decrease the backlight by 5% otherwise
         brightnessctl set 5%-
-        brightnessctl -d amdgpu_bl0 set 5%-
+        brightnessctl -d amdgpu_bl1 set 5%-
     fi
     send_notification ;;
 *)  # print error
