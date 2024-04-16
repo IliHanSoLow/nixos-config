5a1710ef56168f473bf4537b2553838cebf8101e
IliHanSoLow <ilian-ode@gmx.de>
2024-04-16 17:15:19 +0200

    227 current  2024-04-16 17:15:14  24.05.20240403.08b9151  6.6.23                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -291,10 +291,9 @@ in {
   };
 
   #Hyprland
-  environment.sessionVariables = lib.mkForce {
+  environment.sessionVariables = {
     WL_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
   };
 
   xdg.portal.enable = true;
