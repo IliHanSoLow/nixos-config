33bddc3c6bf3175b8567646138c6ea8db1aa15e6
IliHanSoLow <ilian-ode@gmx.de>
2024-04-17 16:26:54 +0200

    241 current  2024-04-16 21:09:38  24.05.20240403.08b9151  6.6.23                          *

--- hosts/default/ilian.nix
+++ hosts/default/ilian.nix
@@ -142,9 +142,9 @@
       provider = "manual";
       latitude = 49.35202;
       longitude = 11.31154;
-      settings.general = lib.mkForce {
-        temp-day = 6500;
-        temp-night = 3700;
+      settings.general = {
+        temp-day = lib.mkForce 6500;
+        temp-night = lib.mkForce 3700;
       };
     };
   };
