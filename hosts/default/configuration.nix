c0af5d152357d931b5c0eabfd1e1144d753b9823
IliHanSoLow <ilian-ode@gmx.de>
2024-05-02 17:54:57 +0200

    297 current  2024-05-02 17:54:51  24.05.20240427.58a1abd  6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -86,15 +86,15 @@ in {
   };
   */
   # Ragnar WM
-  /*
-     nixpkgs.overlays = [
+
+  nixpkgs.overlays = [
     (final: prev: {
       ragnarwm = prev.ragnarwm.overrideAttrs (
         old: {src = /home/ilian/dotfiles/nixos/git_packages/Ragnar;}
       );
     })
   ];
-  */
+
   services.xserver.windowManager.ragnarwm = {
     enable = true;
     package = pkgs.ragnarwm;
