fc782b9fb86820ba3a183bd7beccdaa3c18262c5
IliHanSoLow <ilian-ode@gmx.de>
2024-05-02 15:06:00 +0200

    291 current  2024-05-02 14:33:09  24.05.20240427.58a1abd  6.6.28                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -20,7 +20,7 @@
       url = "github:outfoxxed/hy3";
       inputs.hyprland.follows = "hyprland";
     };
-    ragnar.url = "git+file://./../git_packages/Ragnar";
+    ragnar.url = "git+file://../git_packages/Ragnar";
   };
 
   outputs = {
