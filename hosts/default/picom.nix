2c8f3e2da78a518961f1a04d134875468d77fbda
IliHanSoLow <ilian-ode@gmx.de>
2024-05-06 13:28:26 +0200

    pixom.nix added

---
+++ hosts/default/picom.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  fetchFromGitHub,
+  ...
+}: let
+  mypicom = pkgs.picom.overrideAttrs (old: rec {
+    src = fetchFromGitHub {
+      owner = "pijulius";
+      repo = "picom";
+      rev = "e7b14886ae644aaa657383f7c4f44be7797fd5f6"; # This is the commit hash
+      fetchSubmodules = true;
+      sha256 = "sha256-YQVp5HicO+jbvCYSY+hjDTnXCU6aS3aCvbux6NFcJ/Y=";
+    };
+    buildInputs = old.buildInputs ++ [pkgs.pcre.dev];
+  });
+in {
+  environment.systemPackages = with pkgs; [
+    mypicom
+  ];
+}
