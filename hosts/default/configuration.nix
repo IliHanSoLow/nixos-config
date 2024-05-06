5a70c95d3468660384c60237a9109f5141a8e2af
IliHanSoLow <ilian-ode@gmx.de>
2024-05-06 13:17:49 +0200

    321 current  2024-05-05 22:47:51  24.05.20240427.58a1abd  6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -305,6 +305,7 @@ in {
         repo = "picom";
         rev = "e7b14886ae644aaa657383f7c4f44be7797fd5f6"; # This is the commit hash
         fetchSubmodules = true;
+        sha256 = "sha256-YQVp5HicO+jbvCYSY+hjDTnXCU6aS3aCvbux6NFcJ/Y=";
       };
     })
     alacritty
