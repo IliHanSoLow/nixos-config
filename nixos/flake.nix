eb64235030e128c4eec622ce36de994c20d39983
IliHanSoLow <ilian-ode@gmx.de>
2024-05-07 16:39:11 +0200

    328 current  2024-05-06 18:51:41  24.05.20240505.25865a4  6.6.30                          *

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -98,6 +98,7 @@
         ./../hosts/default/vm.nix
         # ./../hosts/default/theme.nix
         ./../hosts/default/cursor_theme.nix
+        ./../hosts/default/ragnar.nix
         ./../hosts/default/picom.nix
 
         inputs.home-manager.nixosModules.default
