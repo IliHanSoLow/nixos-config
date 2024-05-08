72d9f1127f33acdafca4405d872e6de8a86c8b5e
IliHanSoLow <ilian-ode@gmx.de>
2024-05-08 19:27:35 +0200

    336 current  2024-05-08 19:27:28  24.05.20240505.25865a4  6.6.30                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -71,7 +71,7 @@ in {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager = {
     gdm = {
-      enable = false;
+      enable = true;
       wayland = false;
     };
     startx.enable = true;
