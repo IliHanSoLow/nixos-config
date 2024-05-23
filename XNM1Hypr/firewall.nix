3b75401b16aa4ccaeafe777d2b0a45a5d66cc00e
IliHanSoLow <ilian-ode@gmx.de>
2024-05-23 12:51:50 +0200

    350 current  2024-05-23 12:51:45  24.05.20240511.062ca2a  6.6.30                          *

--- XNM1Hypr/firewall.nix
+++ XNM1Hypr/firewall.nix
@@ -4,5 +4,5 @@
   networking.firewall.allowedTCPPorts = [53317];
   # networking.firewall.allowedUDPPorts = [ 3000 ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = true;
+  networking.firewall.enable = false;
 }
