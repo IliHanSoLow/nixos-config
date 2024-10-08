2f06bec193e5ebb8100b671bb69002b03fe926d5
IliHanSoLow <ilian-ode@gmx.de>
2024-10-08 09:36:09 +0200

    430 current  2024-10-08 09:36:04  24.11.20240929.06cf0e1  6.6.52                          *

--- hosts/default/ilian.nix
+++ hosts/default/ilian.nix
@@ -125,6 +125,7 @@
       calc
       zap
       chatterino2
+      calibre
     ]
     ++ (
       with pkgs.nur; [
@@ -221,21 +222,21 @@
   };
 
   /*
-     dconf.settings = {
-    "org/gnome/desktop/background" = {
-      picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
-    };
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+  dconf.settings = {
+  "org/gnome/desktop/background" = {
+  picture-uri-dark = "file://${pkgs.nixos-artwork.wallpapers.nineish-dark-gray.src}";
+  };
+  "org/gnome/desktop/interface" = {
+  color-scheme = "prefer-dark";
+  };
   };
 
   gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome.gnome-themes-extra;
-    };
+  enable = true;
+  theme = {
+  name = "Adwaita-dark";
+  package = pkgs.gnome.gnome-themes-extra;
+  };
   };
   */
 }
