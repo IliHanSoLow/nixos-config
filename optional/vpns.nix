e41df8f8c9bf75142b1fe1265429db6e020c514c
IliHanSoLow <ilian-ode@gmx.de>
2024-10-16 22:52:16 +0200

    456 current  2024-10-16 22:52:13  24.11.20240929.06cf0e1  6.6.52                          *

--- optional/vpns.nix
+++ optional/vpns.nix
@@ -3,10 +3,10 @@
     sopsFile = ../hosts/common/secrets/secrets.yaml;
     neededForUsers = true;
   };
-  networking.wireguard.interfaces = {
+  networking.wg-quick.interfaces = {
     NLFREE387043 = {
-      ips = ["10.2.0.2/32"];
-      # dns = ["10.2.0.1"];
+      address = ["10.2.0.2/32"];
+      dns = ["10.2.0.1"];
       listenPort = 51820;
       privateKeyFile = config.sops.secrets.NLFREE387043.path;
 
@@ -15,6 +15,7 @@
           publicKey = "lqxGpBwoRUrxKpUvlq3VhJ9SCHgt8N5KxcGEfuz+Vys=";
           allowedIPs = ["0.0.0.0/0"];
           endpoint = "169.150.196.71:51820";
+          persistentKeepalive = 25;
         }
       ];
     };
