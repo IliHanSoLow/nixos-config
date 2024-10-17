1fdff56dd6c4d3b77e443eb77cb3f4a98bdfc181
IliHanSoLow <ilian-ode@gmx.de>
2024-10-17 09:28:48 +0200

    461 current  2024-10-17 09:28:41  24.11.20240929.06cf0e1  6.6.52                          *

--- hosts/default/ilian.nix
+++ hosts/default/ilian.nix
@@ -182,7 +182,7 @@
   #  /etc/profiles/per-user/ilian/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+    # QT_QPA_PLATFORMTHEME = "qt5ct";
     # EDITOR = "emacs";
   };
   services = {
