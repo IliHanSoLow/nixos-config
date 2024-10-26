760edf805db0a1292f92b60b6cef23e93d4ffbb6
IliHanSoLow <ilian-ode@gmx.de>
2024-10-26 12:37:51 +0200

    481 current  2024-10-24 23:31:25  24.11.20241023.2768c7d  6.6.58                          *

--- XNM1Hypr/time.nix
+++ XNM1Hypr/time.nix
@@ -2,4 +2,6 @@
   # Set your time zone.
   time.hardwareClockInLocalTime = true;
   time.timeZone = "Europe/Berlin";
+  services.ntp.enable = true;
+  networking.timeServers = options.networking.timeServers.default ++ ["de.pool.ntp.org"];
 }
