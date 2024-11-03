765140ab28fc06475d4cce2ebea81c60b27bf33c
IliHanSoLow <ilian-ode@gmx.de>
2024-11-03 21:24:28 +0100

    499 current  2024-11-03 21:11:16  24.05.20241030.080166c  6.6.58                          *

--- nixos-conf/locales.nix
+++ nixos-conf/locales.nix
@@ -10,14 +10,14 @@
   networking.timeServers = options.networking.timeServers.default ++ ["de.pool.ntp.org"];
 
   # Select internationalisation properties.
-  i18n.supportedLocales = [
+  ilian.supportedLocales = [
     "en_US.UTF-8/UTF-8"
     "de_DE.UTF-8/UTF-8"
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  ilian.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
+  ilian.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
     LC_MEASUREMENT = "de_DE.UTF-8";
