cbb562420386ca802a4bb02a1552d7dc6bf07625
IliHanSoLow <ilian-ode@gmx.de>
2024-11-06 22:11:47 +0100

    515 current  2024-11-06 21:53:12  24.05.20241030.080166c  6.6.58                          *

--- nixos-conf/dns.nix
+++ nixos-conf/dns.nix
@@ -2,30 +2,51 @@
   # Enable Encrypted DNS
   networking = {
     nameservers = ["127.0.0.1" "::1"];
+
+    # Allow systemd-resolved to handle DNS fallback
+    dns.resolvconf = true;
+
     # If using dhcpcd:
     dhcpcd.extraConfig = "nohook resolv.conf";
     # If using NetworkManager:
-    networkmanager.dns = "none";
+    networkmanager.dns = "systemd-resolved";
   };
 
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      ipv6_servers = true;
-      require_dnssec = true;
+  services = {
+    dnscrypt-proxy2 = {
+      enable = true;
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
 
-      sources.public-resolvers = {
-        urls = [
-          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+
+        # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+        server_names = [
+          "cloudflare"
+          "cloudflare-ipv6"
+          "cloudflare-security"
+          "cloudflare-security-ipv6"
+          "adguard-dns-doh"
+          "mullvad-adblock-doh"
+          "mullvad-doh"
+          "nextdns"
+          "nextdns-ipv6"
+          "quad9-dnscrypt-ipv4-filter-pri"
+          "google"
+          "google-ipv6"
+          "ibksturm"
         ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
-
-      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
-      server_names = ["cloudflare" "cloudflare-ipv6" "cloudflare-security" "cloudflare-security-ipv6" "adguard-dns-doh" "mullvad-adblock-doh" "mullvad-doh" "nextdns" "nextdns-ipv6" "quad9-dnscrypt-ipv4-filter-pri" "google" "google-ipv6" "ibksturm"];
     };
+    resolved.enable = true;
   };
 
   systemd.services.dnscrypt-proxy2.serviceConfig = {
