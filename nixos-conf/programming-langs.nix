8a8fffe94305ce652b9b295e03d8af32763d1e5d
IliHanSoLow <ilian-ode@gmx.de>
2025-01-21 23:02:56 +0100

    544 current  2025-01-17 11:32:02  24.11.20250106.3f0a8ac  6.6.69                          *

--- nixos-conf/programming-langs.nix
+++ nixos-conf/programming-langs.nix
@@ -3,7 +3,7 @@
     go
     gopls
     (python3Full.withPackages (ps: with ps; [pygobject3 gobject-introspection pyqt6-sip]))
-    nodePackages_latest.nodejs
+    nodejs
     unstable.deno
     lua
     zig
